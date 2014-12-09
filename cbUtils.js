21cc4f287faf5194ae52c5d9395d86fd8c9258da
amymc <amy.mccarthy2@mail.dcu.ie>
2014-12-09 03:07:40 -0500

    fixed indentation

--- cbUtils.js
+++ cbUtils.js
@@ -1,24 +1,24 @@
 var cbApi = (function  () {
 
   var api = {},
-	  baseUrl = 'http://api.chartbeat.com/',
-	  path = 'live/toppages/v3/',
-	  apiKey = '317a25eccba186e0f6b558f45214c0e7',
-	  host = 'gizmodo.com';
+      baseUrl = 'http://api.chartbeat.com/',
+      path = 'live/toppages/v3/',
+      apiKey = '317a25eccba186e0f6b558f45214c0e7',
+      host = 'gizmodo.com';
 
   api.getData = function(callback) {
-	var requestUrl = baseUrl + path + '?apikey=' + apiKey + '&host=' + host;
-	var request = new XMLHttpRequest();
-	request.open('get', requestUrl, true);
-	request.onload = function(e) {
-	  var response = request.response;
-	  response = JSON.parse(response);
-	  callback(response);
-	};
-	request.onerror = function(e) {
-	  callback(request.response, e);
-	};
-	request.send();
+    var requestUrl = baseUrl + path + '?apikey=' + apiKey + '&host=' + host;
+    var request = new XMLHttpRequest();
+    request.open('get', requestUrl, true);
+    request.onload = function(e) {
+      var response = request.response;
+      response = JSON.parse(response);
+      callback(response);
+    };
+    request.onerror = function(e) {
+      callback(request.response, e);
+    };
+    request.send();
   };
   return api;
 
